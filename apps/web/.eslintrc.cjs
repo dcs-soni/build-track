92d99bdc734656de153f8ef06c832f17ae369e48
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2026-02-23 12:34:22 +0530

    fix: add eslint deps and configs to api and web apps

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,27 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react-hooks", "react-refresh"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+  },
+};
